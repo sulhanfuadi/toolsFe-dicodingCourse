caeafd512b85c1959162b75f79c609dfe722da6d
sulhanfuadi <sulhanfuadi096@gmail.com>
2024-06-26 13:57:33 +0700

    add lit localization project

---
+++ lit-localization-project/src/generated/locales/es.js
@@ -0,0 +1,23 @@
+
+    // Do not modify this file by hand!
+    // Re-generate this file by running lit-localize
+
+    import {html} from 'lit';
+    import {str} from '@lit/localize';
+
+    /* eslint-disable no-irregular-whitespace */
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+
+    export const templates = {
+      'hf60f020b62b709fc': html`
+          Visite
+          <a href="https://www.dicoding.com/" target="_blank">
+          este sitio
+          </a>
+          para ser un programador profesional
+        `,
+'s6dfd1a978586d8de': str`Bienvenidas, ${0}!
+        `,
+'sacac6b633604cb76': `Espero que estés en buenas condiciones.`,
+    };
+  
---
+++ lit-localization-project/src/generated/locales/id.js
@@ -0,0 +1,23 @@
+
+    // Do not modify this file by hand!
+    // Re-generate this file by running lit-localize
+
+    import {html} from 'lit';
+    import {str} from '@lit/localize';
+
+    /* eslint-disable no-irregular-whitespace */
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+
+    export const templates = {
+      'hf60f020b62b709fc': html`
+          Silakan kunjungi
+          <a href="https://www.dicoding.com/" target="_blank">
+          situs ini
+          </a>
+          untuk menjadi programmer profesional
+        `,
+'s6dfd1a978586d8de': str`Selamat datang, ${0}!
+        `,
+'sacac6b633604cb76': `Saya harap Anda dalam kondisi baik`,
+    };
+  
