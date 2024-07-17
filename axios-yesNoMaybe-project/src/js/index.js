eab2a1b7f4f60ca4f7e27620a1f5cdbed6276a42
sulhanfuadi <sulhanfuadi096@gmail.com>
2024-07-17 14:36:44 +0700

    update axios-yesNoMaybe-project

---
+++ axios-yesNoMaybe-project/src/js/index.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+
+const CONFIG = {
+  BASE_URL: "https://yesno.wtf/api",
+  BASE_URL_WITH_FORCED: (forcedValue) =>
+    `https://yesno.wtf/api?force=${forcedValue}`,
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("#questionForm");
+  const forcedAnswerCheckbox = document.querySelector("#forcedAnswerCheckbox");
+  const forcedAnswerContainer = document.querySelector(
+    "#forcedAnswerContainer"
+  );
+  const forcedAnswerRadios = document.querySelectorAll(
+    'input[name="forcedAnswer"]'
+  );
+  const textAnswer = document.querySelector("#textAnswer");
+
+  function getCheckedRadios(radioElements) {
+    return [...radioElements].filter((item) => item.checked)[0];
+  }
+
+  function setLoading(element) {
+    element.textContent = "Loading...";
+  }
+
+  form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const checkedForcedAnswerRadio = getCheckedRadios(forcedAnswerRadios);
+    let endpoint = CONFIG.BASE_URL;
+    if (checkedForcedAnswerRadio) {
+      endpoint = CONFIG["BASE_URL_WITH_FORCED"](checkedForcedAnswerRadio.value);
+    }
+
+    setLoading(textAnswer);
+    const response = await axios(endpoint);
+    textAnswer.textContent = response.data.answer;
+  });
+
+  forcedAnswerCheckbox.addEventListener("change", (event) => {
+    const isChecked = event.target.checked;
+
+    if (isChecked) {
+      forcedAnswerContainer.style.display = "block";
+    } else {
+      forcedAnswerContainer.style.display = "none";
+      getCheckedRadios(forcedAnswerRadios).checked = false;
+    }
+  });
+});
