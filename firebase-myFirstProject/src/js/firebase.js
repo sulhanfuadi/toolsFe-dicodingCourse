a35333eaaf20955a5b9f7b0c4ac8fcd0772de8c4
sulhanfuadi <sulhanfuadi096@gmail.com>
2024-07-18 17:16:49 +0700

    update firebase-myFirstProject

---
+++ firebase-myFirstProject/src/js/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD6ejp6_dTV5GY_2XWZDB1UoEYAMTaaq6Q",
+  authDomain: "my-first-project-18072024.firebaseapp.com",
+  projectId: "my-first-project-18072024",
+  storageBucket: "my-first-project-18072024.appspot.com",
+  messagingSenderId: "247710539837",
+  appId: "1:247710539837:web:f46582517d6c07e434ca54",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export { app };
